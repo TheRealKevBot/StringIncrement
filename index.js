d22c9e04970e925a554267bafe44d4e3a8c8087a
TheRealKevBot <Kglas001@gmail.com>
2021-06-17 22:02:39 -0600

    First

---
+++ index.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript 
+
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+
+function incrementString (strng) {
+    // return incrementedString
+  } 
+
+console.log(incrementString("foobar000"), "foobar001");
+console.log(incrementString("foo"), "foo1");
+console.log(incrementString("foobar001"), "foobar002");
+console.log(incrementString("foobar99"), "foobar100");
+console.log(incrementString("foobar099"), "foobar100");
+console.log(incrementString(""), "1");
